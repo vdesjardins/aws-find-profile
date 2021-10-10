90050f05f35e7a6050ae96e50e60bd864fce7c1f
Vincent Desjardins <vincent.desjardins@lacapitale.com>
2021-10-10 23:30:28 +0000

    feat: add process profile name

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Clap;
 use configparser::ini::{Ini, IniDefault};
 use shellexpand;
+use std::collections::HashMap;
 use std::error::Error;
 
 #[derive(Clap, Debug)]
@@ -9,6 +10,9 @@ struct Account {
     #[clap(short, long)]
     id: String,
 }
+
+const SSO_ACCOUNT_ID: &str = "sso_account_id";
+const CREDENTIAL_PROCESS: &str = "credential_process";
 
 fn main() -> Result<(), Box<dyn Error>> {
     let account = Account::parse();
@@ -28,20 +32,41 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
 
     let mut config = Ini::new_from_defaults(config_default.clone());
-    let map = config.load(&*config_path)?;
+    let config = config.load(&*config_path)?;
 
-    let mut section = String::from("");
-    for (k, v) in map {
-        if let Some(Some(id)) = v.get("sso_account_id") {
-            if id == &account.id {
-                section = k;
-                break;
+    let profile_name = find_profile_by_account_id(&account.id, &config);
+    let proc_profile = find_process_profile(profile_name, &config);
+
+    println!("name\t{}", profile_name);
+    println!("process\t{}", proc_profile);
+
+    Ok(())
+}
+
+fn find_profile_by_account_id<'a>(
+    account_id: &'a str,
+    config: &'a HashMap<String, HashMap<String, Option<String>>>,
+) -> &'a str {
+    for (section, values) in config {
+        if let Some(Some(id)) = values.get(SSO_ACCOUNT_ID) {
+            if id == &account_id {
+                return section.strip_prefix("profile ").unwrap();
             }
         }
     }
+    ""
+}
 
-    let profile_name = section.strip_prefix("profile ").unwrap();
-    println!("{}", profile_name);
-
-    Ok(())
+fn find_process_profile<'a>(
+    profile: &'a str,
+    config: &'a HashMap<String, HashMap<String, Option<String>>>,
+) -> &'a str {
+    for (section, values) in config {
+        if let Some(Some(cmd)) = values.get(CREDENTIAL_PROCESS) {
+            if cmd.contains(profile) {
+                return section.strip_prefix("profile ").unwrap();
+            }
+        }
+    }
+    ""
 }
